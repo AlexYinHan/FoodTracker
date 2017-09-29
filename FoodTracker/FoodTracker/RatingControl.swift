50104b7632882dd1bb9514799b6740260984c067
AlexYinHan <411629507@qq.com>
2017-09-29 17:58:07 +0800

    add the rating buttons

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -8,7 +8,29 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
+ // 笔记：加上IBDesignable之后，Interface Builder可以获取到具体的内容，画布就可以知道元素的尺寸和位置，在此例中可以消除“ambiguous layout”的warning
+    
+    // MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0 {
+        didSet{
+            updateButtonSelectionStates()
+        }
+    }
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0){
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5{
+        didSet{
+            setupButtons()
+        }
+    }
+    // 笔记：每次properties改变时，自动更新button的信息
     
     // MARK: Initialization
     
@@ -26,28 +48,112 @@ class RatingControl: UIStackView {
     
     // Button Action
     @objc func ratingButtonTapped(button: UIButton){
-        print("Button pressed")
+        guard let index = ratingButtons.index(of: button) else{
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            //If the selected star represents the current rating, reset the rating to 0
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     // MARK:Private Methods
     
     private func setupButtons() {
         
-        // Create the button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
+        // clear all existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+            /**
+             *笔记：
+             *第一行：先从stack view管理的列表中删除button，这告诉stack view，不用再计算该button的值，此时button依然是stack view的一个subview
+             *第二行：彻底删除了这个button
+             */
+        }
+        ratingButtons.removeAll()
         
-        // Add constrains
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        // Load Button Images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        // 笔记：因为图像素材在应用的主目录下，其实可以直接使用UIImage(named:)加载，但是因为这一段代码还需要在IB中运行（@IBDesignable），所以要明确指定bundle
         
-        // Setup the button action
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        for index in 0..<starCount {
+            // Create the button
+            let button = UIButton()
+            
+            // Set the button images
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
+            //button.backgroundColor = UIColor.red
+            /**
+             *笔记：
+             *button有五种状态：normal, highlighted, focused, selected, and disabled
+             *normal是默认状态
+             */
+            
+            // Add constrains
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            //Set the accessibility label
+            button.accessibilityLabel = "Set \(index + 1) star rating"
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            // 笔记: touchUpInside，当用户触碰并且松开时手指依然在范围才会触发，优于touchdown，因为可以把手指移开来取消点击
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+            
+        }
         
-        // Add thebutton to the stack
-        addArrangedSubview(button)
-        
+        updateButtonSelectionStates()
+    }
+    
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected
+            button.isSelected = index < rating
+            
+            // Set the hint string for the currently selected star
+            let hintString: String?
+            if rating == index + 1 {
+                hintString = "Tap to reset the rating to zero."
+            } else {
+                hintString = nil
+            }
+            
+            // Calculate the value string
+            let valueString: String
+            switch (rating) {
+            case 0:
+                valueString = "No rating set."
+            case 1:
+                valueString = "1 star set."
+            default:
+                valueString = "\(rating) stars set."
+            }
+            
+            // Assgin the hint string and value string
+            button.accessibilityHint = hintString
+            button.accessibilityValue = valueString
+        }
     }
 
 }
