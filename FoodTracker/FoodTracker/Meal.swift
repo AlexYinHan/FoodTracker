42906a249202ea8abfd83a28c256852f6fb84a0a
AlexYinHan <411629507@qq.com>
2017-10-14 16:41:49 +0800

    Finish learning all the chapters, everything works fine.

--- FoodTracker/FoodTracker/Meal.swift
+++ FoodTracker/FoodTracker/Meal.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
-class Meal {
+import os.log
+
+class Meal: NSObject, NSCoding {
     
     //MARK: Properties
     
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    //MARK: Archinving Paths
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
+    
+    //MARK: Types
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
     
     //MARK: Initialization
     
@@ -33,5 +49,39 @@ class Meal {
         self.photo = photo
         self.rating = rating
         
+    }
+    
+    //MARK: NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        /**
+         Learing Notes:
+         The required modifier means this initializer must be implemented on every subclass, if the subclass defines its own initializers.
+         The convenience modifier means that this is a secondary initializer, and that it must call a designated initializer from the same class.
+         The question mark (?) means that this is a failable initializer that might return nil.
+         */
+        
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        // Because photo is an optional property of Meal, just use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        /**
+         * Learning Notes:
+         * Note that those are actually two different functions.
+         */
+        
+        // Must call designated initializer
+        self.init(name: name, photo: photo, rating: rating)
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
     Apple Developer 上的学习材料,使用Xcode9和Swift3制作一个可以存储食谱的app<br />
     [Start Developing iOS Apps(Swift)](https://developer.apple.com/library/content/referencelibrary/GettingStarted/DevelopiOSAppsSwift/index.html#//apple_ref/doc/uid/TP40015214-CH2-SW1)<br />
     用于个人学习记录<br />
+    在设备上白屏的解决方法：project clean [stackoverflow-abort with payload](https://stackoverflow.com/questions/41549131/dyld-abort-with-payload-error-on-iphone-5s-device)
